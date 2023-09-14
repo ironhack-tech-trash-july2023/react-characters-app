75e36c258a08a5ec815d248c897124496dcaa044
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-14 17:13:05 +0200

    routing + functionality to display character details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import axios from "axios";
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
 
 import './App.css'
+import CharacterDetails from './components/CharacterDetails';
 
 
 function App() {
@@ -19,27 +21,40 @@ function App() {
   }, []);
 
 
+
   const renderList = () => {
+    if (characters === null) {
+      return <p>loading....</p>
+    }
+
     return characters.map((characterObj, index) => {
-          return (
-            <div key={index} >
-              <h3>{characterObj.name}</h3>
-              <p>{characterObj.occupation}</p>
-            </div>
-          )
-        })
+      return (
+        <section key={index} className='card' >
+          <h3>{characterObj.name}</h3>
+          <p>{characterObj.occupation}</p>
+          <Link to={"/characters/" + characterObj.id}>More Details</Link>
+        </section>
+      )
+    })
   }
 
 
   return (
     <>
-      { characters && <h2>Number of characters in the API: {characters.length}</h2>}
 
-      {
-        characters === null
-          ? <p>loading....</p>
-          : renderList()
-      }
+      <nav>
+        <NavLink to="/">Home</NavLink> |
+        <NavLink to="/contact">Contact</NavLink> |
+        <NavLink to="/about">About</NavLink> |
+      </nav>
+
+      <Routes>
+        <Route path='/' element={renderList()} />
+        <Route path='/contact' element={<p>this will be the CONTACT PAGE</p>} />
+        <Route path='/about' element={<p>this will be the ABOUT PAGE</p>} />    
+        <Route path='/characters/:characterId' element={<CharacterDetails />} />
+      </Routes>
+      
     </>
   )
 }
