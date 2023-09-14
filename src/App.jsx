22722cbf91fd6972095792e935a9f5e9150b2744
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-14 14:45:53 +0200

    (useEffect) display list of characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from "axios";
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://ih-crud-api.herokuapp.com/characters")
+      .then(response => {
+        setCharacters(response.data);
+      })
+      .catch((e) => {
+        console.log("error getting characters from the API....", e);
+      });
+  }, []);
+
+
+  const renderList = () => {
+    return characters.map((characterObj, index) => {
+          return (
+            <div key={index} >
+              <h3>{characterObj.name}</h3>
+              <p>{characterObj.occupation}</p>
+            </div>
+          )
+        })
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      { characters && <h2>Number of characters in the API: {characters.length}</h2>}
+
+      {
+        characters === null
+          ? <p>loading....</p>
+          : renderList()
+      }
     </>
   )
 }
